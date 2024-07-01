36564cba49dcac99c40757bb7da21972827c8b46
Waseb-untwale <untwalewaseb@gmail.com>
2024-07-01 20:30:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AppName from './Component/AppName'
+import ClockSlogan from './Component/ClockSlogan'
+import ClockTime from './Component/ClockTime'
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+
+  return (
+    <center>
+     <AppName/>
+     <ClockSlogan/>
+     <ClockTime/>
+    </center>
+  )
+}
+
+export default App
